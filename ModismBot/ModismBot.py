8286fa3388f44fc32a408d0648b726c8aff31bc0
Alex Hirschfeld <alex@d-qoi.com>
2016-12-28 01:31:21 -0500

    Got mongoDB working with modism, unfortunantly It now requires Mongo. Use the previous commit of this bot if you don't want mongo (though there is a CLI arg for it)

--- ModismBot/ModismBot.py
+++ ModismBot/ModismBot.py
@@ -7,22 +7,27 @@
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from random import choice
+from pymongo import MongoClient
 import logging
 import argparse
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-	level=logging.INFO)
+	level=logging.INFO) # To make sure that it is just pushing modism debug into the log
 logger = logging.getLogger(__name__)
 
-
+# All of this should be moved, but lazy coder is lazy
 parser = argparse.ArgumentParser()
 parser.add_argument('auth', type=str, help="The Auth Token given by Telegram's @botfather")
 parser.add_argument('-l','--llevel', default='debug', choices=['debug','info','warn','none'], help='Logging level for the logger, default = debug')
 logLevel = {'none':logging.NOTSET,'debug':logging.DEBUG,'info':logging.INFO,'warn':logging.WARNING}
 parser.add_argument('-muri','--MongoURI', default='mongodb://localhost:27017', help="The MongoDB URI for connectiong and auth")
-parser.add_argument('-mdb','--MongoDB', default='STT', help="The MongoDB Database that this will use")
+parser.add_argument('-mdb','--MongoDB', default='modism', help="The MongoDB Database that this will use")
 args = parser.parse_args()
 
+# Being ULTRA lazy
+mClient = MongoClient(args.MongoURI)
+mDatabase = mClient[args.MongoDB]
+mCollection = mDatabase.groups
 
 def start(bot, update):
 	logger.debug('User "%s (%s)" /start' % (update.message.from_user.username, update.message.from_user.id))
@@ -38,36 +43,39 @@ def help(bot, update):
 		update.message.reply_text("Why would you call this command, what were you expecting?")
 
 
-def addSelfToGroup(bot, update, chat_data):
-	if update.message.new_chat_member.username == bot.username and (update.message.chat.type == 'group' or update.message.chat.type == 'supergroup'):
-		logger.info("Bot was added to %s (%s)" % (update.message.chat.title, update.message.chat.id))
-		chat_data['mod_messages'] = list()
+# def addSelfToGroup(bot, update):
+# 	if update.message.new_chat_member.username == bot.username and (update.message.chat.type == 'group' or update.message.chat.type == 'supergroup'):
+# 		logger.info("Bot was added to %s (%s)" % (update.message.chat.title, update.message.chat.id))
+# 		chat_data['mod_messages'] = list()
 
 
-def receiveMessage(bot, update, chat_data):
+def receiveMessage(bot, update):
 	if update.message.chat.type == 'group' or update.message.chat.type == 'supergroup' and not update.message.chat.all_members_are_admins:
 		adminIDs = [chatmember.user.username for chatmember in update.message.chat.get_administrators()]
 		if update.message.from_user.username in adminIDs:
 			logger.debug("Admins of %s: %s" % (update.message.chat.title, str(adminIDs)))
 			logger.debug("%s sent %s to %s" % (update.message.from_user.username, update.message.text, update.message.chat.title))
-			if not 'mod_chat' in chat_data:
-				chat_data['mod_chat'] = list()
-			chat_data['mod_chat'].append(update.message.message_id)
-			logger.debug("Number of messages stored: %s" % (str(len(chat_data['mod_chat']))))
+			# if not 'mod_chat' in chat_data:
+			# 	chat_data['mod_chat'] = list()
+			# chat_data['mod_chat'].append(update.message.message_id)
+			mCollection.find_one_and_update({'_id':update.message.chat.id},{'$inc' : {'count':1}, '$push' : {"messages":update.message.message_id}}, upsert=True)
+			#logger.debug("Number of messages stored: %s" % (str(len(chat_data['mod_chat']))))
 
 
-def modism(bot, update, chat_data):
-	if not 'mod_chat' in chat_data or len(chat_data['mod_chat']) == 0:
-		chat_data['mod_chat'] = list()
+def modism(bot, update):
+	findRes = mCollection.find({'_id':update.message.chat.id})
+	if findRes.count() == 0:
 		update.message.reply_text("The message list is empty, this bot was probably restarted.")
-	elif update.message.chat.type == 'group' or update.message.chat.type == 'supergroup' and not update.message.chat.all_members_are_admins and chat_data['mod_chat']:
-		bot.forwardMessage(chat_id = update.message.chat.id, from_chat_id = update.message.chat.id, message_id = choice(chat_data['mod_chat']))
+	elif update.message.chat.type == 'group' or update.message.chat.type == 'supergroup' and not update.message.chat.all_members_are_admins:
+		data = findRes.next()['messages']
+		bot.forwardMessage(chat_id = update.message.chat.id, from_chat_id = update.message.chat.id, message_id = choice(data))
 
-def modismStats(bot, update, chat_data):
-	if 'mod_chat' in chat_data:
-		update.message.reply_text("Messages stored: %s" % str(len(chat_data['mod_chat'])))
+def modismStats(bot, update):
+	findRes = mCollection.find({'_id':update.message.chat.id})
+	#if 'mod_chat' in chat_data:
+	if findRes.count() > 0:
+		update.message.reply_text("Messages stored: %s" % str(findRes.next()['count']))
 	else:
-		chat_data['mod_chat'] = list()
 		update.message.reply_text("The message list is empty, this bot was probably restarted.")
 
 
@@ -83,11 +91,11 @@ def main():
 
 	dp.add_handler(CommandHandler('start', start))
 	dp.add_handler(CommandHandler('help', help))
-	dp.add_handler(CommandHandler('modism', modism, pass_chat_data=True))
-	dp.add_handler(CommandHandler('modismstats', modismStats, pass_chat_data=True))
+	dp.add_handler(CommandHandler('modism', modism))
+	dp.add_handler(CommandHandler('modismstats', modismStats))
 
-	dp.add_handler(MessageHandler(Filters.text, receiveMessage, pass_chat_data=True))
-	dp.add_handler(MessageHandler(Filters.status_update, addSelfToGroup, pass_chat_data=True))
+	dp.add_handler(MessageHandler(Filters.text, receiveMessage))
+	#dp.add_handler(MessageHandler(Filters.status_update, addSelfToGroup)
 	dp.add_error_handler(error)
 
 	updater.start_polling()
