58757bcf47bd7ba9050be1963d9ab84c6c743225
Alex Hirschfeld <alex@d-qoi.com>
2017-02-04 01:14:01 -0500

    SpeachToTextBot

--- SpeachToTextBot/SpeachToTextBot.py
+++ SpeachToTextBot/SpeachToTextBot.py
@@ -5,12 +5,12 @@
 
 import argparse
 import logging
+import subprocess
 import speech_recognition as sr
-from uuid import uuid4
-from pymongo import MongoClient
-from tempfile import TemporaryFile
-from telegram import InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler, Filters, MessageHandler
+from os import getcwd
+from tempfile import NamedTemporaryFile
+from telegram import TelegramError
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
 
 AUTHTOKEN = None
 
@@ -20,6 +20,8 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 r = sr.Recognizer()
+
+cwd = getcwd()
 
 
 def start(bot, update):
@@ -38,21 +40,28 @@ def help(bot, update):
 #     logger.info(query)
 
 def receiveMessage(bot, update):
-    logger.debug("Recieved a message")
+    logger.debug("Received a message")
     if update.message.voice:
         logger.debug("Is a voice message")
         voiceID = update.message.voice.file_id
         
-        try:
-            file = bot.getFile(file_id = voiceID)
-            logger.debug("Got the file")
-        except telegram.TelegramError:
-            logger.warn("Failed to get a file.")
-
-        with TemporaryFile() as tfile:
-            file.download(out=tfile)
+        with NamedTemporaryFile(suffix='.ogg', dir=cwd) as inFile, NamedTemporaryFile(suffix='.wav', dir=cwd) as outFile:
+            try:
+                file = bot.getFile(file_id = voiceID)
+                logger.debug("Got the file")
+            except TelegramError:
+                logger.warn("Failed to get a file.")
+                        
+        
+            file.download(inFile.name)
+            #file.download()
             logger.debug("Downloaded the file")
-            with sr.AudioFile(tfile) as audioFile:
+            print(inFile.name)
+            command = ['ffmpeg','-y','-sample_fmt','s16','-i', inFile.name, outFile.name]
+            subprocess.run(command, stdout=subprocess.PIPE)
+            #logger.debug('oggdec output: \n %s' % p)
+            
+            with sr.AudioFile(outFile) as audioFile:
                 audio = r.record(audioFile)
                 logger.debug("Attempting to translate")
                 try:
@@ -62,9 +71,11 @@ def receiveMessage(bot, update):
                     print("Google Speech Recognition could not understand audio")
                 except sr.RequestError as e:
                     print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-        update.message.reply_text(recognized)
-    
+        
+        alts = sorted(recognized['alternative'], key=lambda conf: conf['confidence'], reverse=True)
+        reply_text = "Confidence: %s\nText:\n%s"%(alts[0]['confidence'], alts[0]['transcript'])
+        update.message.reply_text(reply_text)
+     
 
 def error(bot, update, error):
     logger.warn('Update "%s" cause error "%s"' %(update, error))
@@ -75,8 +86,10 @@ def startFromCLI():
     parser.add_argument('auth', type=str, help="The Auth Token given by Telegram's @botfather")
     parser.add_argument('-l','--llevel', default='debug', choices=['debug','info','warn','none'], help='Logging level for the logger, default = debug')
     logLevel = {'none':logging.NOTSET,'debug':logging.DEBUG,'info':logging.INFO,'warn':logging.WARNING}
+    
     args = parser.parse_args()
-
+    logger.setLevel(logLevel[args.llevel])
+    
     AUTHTOKEN = args.auth
 
 def main():
